2d553a02a3b8139ed571e9356dae1688cfeea389
Admin <abdutokt2004>
2022-07-14 09:56:14 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        List<String> music = new ArrayList<>();
+        String line1 = "Ветер с моря дул";
+        String line2 = "Нагонял беду";
+        String line3 = "И сказал ты мне";
+        String line4 = "Больше не приду";
+        music.add(line1);
+        music.add(line2);
+        music.add(line3);
+        music.add(line4);
+        System.out.println(music);
+    }
+}
